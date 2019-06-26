ae1bc693580d0f98694e00478a2ad1a124ed06b4
simonequarticelli <quarti87@gmail.com>
2019-06-26 23:21:35 +0200

    init ex php-hotelcrud

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Come vi dicevo a lezione, oggi pomeriggio provate a fare la connessione
+al db da php e provate a fare la prima query di select di tutte le stanze.
+Se tutto va a buon fine, impaginate la tabella (sì, siete autorizzati ad
+usare il tag <table> :rolling_on_the_floor_laughing: ), magari potete aiutarvi
+con bootstrap, se vi fa piacere :wink: -->
+
+<?php
+
+
+
+
+?>
